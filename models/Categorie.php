b055b7648a62fa61323b77c4d33f22778e8c4386
atetheone <atevirran@gmail.com>
2024-06-09 14:41:22 +0000

    Add getters and setters

---
+++ models/Categorie.php
@@ -0,0 +1,37 @@
+<?php
+class Categorie {
+  private $conn;
+  private $table_name = "Categorie";
+
+  private $id;
+  private $libelle;
+
+  public function __construct($db) {
+    $this->conn = $db;
+  }
+
+  public function readOne($id) {
+      $query = "SELECT libelle FROM " . $this->table_name . " WHERE id = :id";
+
+      $stmt = $this->conn->prepare($query);
+      $stmt->bindParam(':id', $id);
+      $stmt->execute();
+      return $stmt->fetch(PDO::FETCH_ASSOC);
+  }
+
+  public function getId() {
+    return $this->id;
+  }
+
+  public function getLibelle() {
+    return $this->libelle;
+  }
+
+  public function setId($id) {
+    $this->id = $id;
+  }
+
+  public function setLibelle($libelle) {
+    $this->libelle = $libelle;
+  }
+}
