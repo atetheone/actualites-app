418a973c112cac2abe78a3950f8d16a2e2f129ca
atetheone <atevirran@gmail.com>
2024-06-09 17:28:58 +0000

    Define CategorieController class

---
+++ controllers/CategorieController.php
@@ -0,0 +1,29 @@
+<?php
+include_once 'config/Database.php';
+include_once 'models/Categorie.php';
+include_once 'models/Article.php';
+
+class CategorieController {
+  private $db;
+  private $category;
+  private $article;
+
+  public function __construct() {
+    $database = new Database();
+    $this->db = $database->getConnection();
+    $this->category = new Categorie($this->db);
+    $this->article = new Article($this->db);
+  }
+
+  public function show($id) {
+    $category = $this->category->readOne($id);
+    $stmt = $this->article->readByCategory($id);
+    $articles = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    include 'views/categories.php';
+  }
+
+  public function getCategories() {
+    $categories = $this->category->readAll();
+    return $categories;
+  }
+}
