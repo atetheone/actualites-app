c902057cfb5ab3f7ffae190c41dd65ac0ef6bd30
atetheone <atevirran@gmail.com>
2024-06-09 17:26:35 +0000

    Update DB confi

--- config/Database.php
+++ config/Database.php
@@ -1,7 +1,7 @@
 <?php
 class Database {
   private $host = "localhost";
-  private $db_name = "tp_ial";
+  private $db_name = "mglsi_news";
   private $username = "root";
   private $password = "";
   public $conn;
