9ac22967205956689bd8ac327ac18b0e1176a83c
atetheone <atevirran@gmail.com>
2024-06-09 17:27:27 +0000

    Add readAll() method

--- models/Categorie.php
+++ models/Categorie.php
@@ -19,6 +19,14 @@ class Categorie {
       return $stmt->fetch(PDO::FETCH_ASSOC);
   }
 
+  public function readAll() {
+    $query = "SELECT id, libelle FROM " . $this->table_name;
+
+    $stmt = $this->conn->prepare($query);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+  }
+
   public function getId() {
     return $this->id;
   }
