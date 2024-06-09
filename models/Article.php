fd5763f73a68845c9564095082f97818ac87d10f
atetheone <atevirran@gmail.com>
2024-06-09 14:35:52 +0000

    Update Article model

--- models/Article.php
+++ models/Article.php
@@ -1,20 +1,36 @@
 <?php
-
 class Article {
   private $conn;
+  private $table_name = "Article";
 
-  public function __construct($conn) {
-    $this->conn = $conn;
+  public $id;
+  public $titre;
+  public $contenu;
+  public $dateCreation;
+  public $categorie;
+
+  public function __construct($db) {
+    $this->conn = $db;
   }
 
+  public function read() {
+    $query = "SELECT Article.titre, Article.contenu, Categorie.libelle 
+              FROM " . $this->table_name . " 
+              JOIN Categorie ON Article.categorie = Categorie.id 
+              ORDER BY Article.dateCreation DESC";
 
+    $stmt = $this->conn->prepare($query);
+    $stmt->execute();
+    return $stmt;
+  }
 
-  public function getAllArticles() {
-    $query = $this->conn->query("SELECT Article.titre, Article.contenu, Categorie.libelle 
-                                 FROM Article 
-                                 JOIN Categorie ON Article.categorie = Categorie.id 
-                                 ORDER BY Article.dateCreation DESC");
+  public function readByCategory($categoryId) {
+    $query = "SELECT titre, contenu FROM " . $this->table_name . " 
+              WHERE categorie = :id ORDER BY dateCreation DESC";
 
-    return $query->fetchAll(PDO::FETCH_ASSOC);
+    $stmt = $this->conn->prepare($query);
+    $stmt->bindParam(':id', $categoryId);
+    $stmt->execute();
+    return $stmt;
   }
 }
