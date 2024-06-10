9ae42605b36928f9c39ecd6e5fbfbf9d374cf556
atetheone <atevirran@gmail.com>
2024-06-10 04:43:46 +0000

    Refactor Categorie model

--- models/Categorie.php
+++ models/Categorie.php
@@ -1,34 +1,11 @@
 <?php
 class Categorie {
-  private $conn;
-  private $table_name = "Categorie";
-
   private $id;
   private $libelle;
 
-  public function __construct($db) {
-    $this->conn = $db;
-  }
-
-  public function readOne($id) {
-      $query = "SELECT libelle FROM " . $this->table_name . " WHERE id = :id";
-
-      $stmt = $this->conn->prepare($query);
-      $stmt->bindParam(':id', $id);
-      $stmt->execute();
-      return $stmt->fetch(PDO::FETCH_ASSOC);
-  }
-
-  public function readAll() {
-    $query = "SELECT id, libelle FROM " . $this->table_name;
-
-    $stmt = $this->conn->prepare($query);
-    $stmt->execute();
-    return $stmt->fetchAll(PDO::FETCH_ASSOC);
-  }
-
-  public function getId() {
-    return $this->id;
+  public function __construct($id, $libelle) {
+    $this->id = $id;
+    $this->libelle = $libelle;
   }
 
   public function getLibelle() {
