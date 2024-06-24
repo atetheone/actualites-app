af3e8062874b5bb8da1225228639677eec8f8606
atetheone <atevirran@gmail.com>
2024-06-24 03:24:18 +0000

    Refactor articles.php to display category name in the heading

--- views/articles.php
+++ views/articles.php
@@ -2,7 +2,7 @@
 	include 'views/templates/header.php';  
 ?>
 
-<h1>Les Articles </h1>
+<h1>Les Articles de categorie <?= $categorie['libelle'] ?></h1>
 
 <div class="articles">
     <?php if (count($articles) > 0): ?>
@@ -10,7 +10,6 @@
             <article>
                 <h2><?= $article['titre'] ?></h2>
                 <p><?= $article['contenu'] ?></p>
-				<p><?=  "Categorie : " . $article['libelle'] ?></p>
                 <small>Publié le: <?= (new DateTime($article['dateCreation']))->format('d/m/Y H:i:s') ?></small>
             </article>
         <?php endforeach; ?>
