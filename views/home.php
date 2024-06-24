a582a2e55a727067c57f3374257ae05cd1f290a0
atetheone <atevirran@gmail.com>
2024-06-24 03:23:28 +0000

    Refactor home.php to display all articles with their categories and publication dates

--- views/home.php
+++ views/home.php
@@ -1,3 +1,19 @@
 <?php include 'templates/header.php'; ?>
-<h1>Accueil</h1>
+
+<h1>Tous les Articles </h1>
+
+<div class="articles">
+    <?php if (count($articles) > 0): ?>
+        <?php foreach ($articles as $article): ?>
+            <article>
+                <h2><?= $article['titre'] ?></h2>
+                <p><?= $article['contenu'] ?></p>
+				<p><?=  "Categorie : " . $article['libelle'] ?></p>
+                <small>Publié le: <?= (new DateTime($article['dateCreation']))->format('d/m/Y H:i:s') ?></small>
+            </article>
+        <?php endforeach; ?>
+    <?php else: ?>
+        <p> Aucun article trouvé.</p>
+    <?php endif; ?>
+</div>
 <?php include 'templates/footer.php'; ?>
