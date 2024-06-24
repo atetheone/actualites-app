2d486ebe4d8e45041ba883a40d6ae26e59ed0ec6
atetheone <atevirran@gmail.com>
2024-06-24 03:24:04 +0000

    Refactor footer.php to add author attribution in the copyright notice

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -1,6 +1,6 @@
 </main>
 <footer>
-    <p>&copy; 2024 MGLSI News</p>
+    <p>&copy; 2024 MGLSI News | <i>Par <a href="https://www.linkedin.com/in/atetheone" target="_blank">Até Tougué Aristide</a></i></p>
 </footer>
 </body>
 </html>
