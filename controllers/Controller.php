e5072bb911166cc4546698f75de560d30da0b74a
atetheone <atevirran@gmail.com>
2024-06-24 03:21:41 +0000

    Refactor Controller by adding categories as an attribute

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -5,21 +5,29 @@ require_once 'daos/CategorieDAO.php';
 class Controller {
   private $articleDAO;
   private $categorieDAO;
+  private $categories;
 
   public function __construct() {
     $this->articleDAO = new ArticleDAO();
     $this->categorieDAO = new CategorieDAO();
+    $this->categories = $this->categorieDAO->getAllCategories();
   }
 
   public function getAllArticles() {
-    $categories = $this->categorieDAO->getAllCategories();
+    $categories = $this->categories;
     $articles = $this->articleDAO->getAllArticles();
-    include 'views/articles.php';
+    include 'views/home.php';
   }
 
   public function getArticleByCategorieId($categorie) {
-    $categories = $this->categorieDAO->getAllCategories();
+    $categories = $this->categories;
+
     $articles =  $this->articleDAO->getArticlesByCategorie($categorie);
     include 'views/articles.php';
+  }
+
+  public function notFoundResource() {
+    $categories = $this->categories;
+    include 'views/404.php';
   }
 }
